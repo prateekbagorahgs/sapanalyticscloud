2b988e495b61cb1af653d27336d7dd9e5e6d6c7f
prateekbagorahgs <143021348+prateekbagorahgs@users.noreply.github.com>
2023-08-26 01:03:05 -0400

    Update QueryChatGPTWidget.js

--- QueryChatGPTWidget.js
+++ QueryChatGPTWidget.js
@@ -1,17 +1,28 @@
-(function() {
-	const template = document.createElement("template");
+var ajaxCall = (prompt) => {
+  return new Promise((resolve, reject) => {
+    const staticResponse = {
+      choices: [{ text: "This is a static response for testing." }],
+    };
+    resolve({ response: staticResponse, status: 200 });
+  });
+};
 
-	template.innerHTML = `
- 		<div id="root" style="width: 100%; height: 100%;">
-      		</div>
-    	`;
-	
-	class MainWebComponent extends HTMLElement {
-		constructor() {
-      			super();
-      			console.log("Hello!")
-			console.log('dataBinding:', dataBinding);
-			}
+(function () {
+  const template = document.createElement("template");
+  template.innerHTML = `
+		<style>
+		</style>
+		<div id="root" style="width: 100%; height: 100%;">
+		</div>
+		`;
+  class MainWebComponent extends HTMLElement {
+    async post(prompt) {
+      const { response } = await ajaxCall(
+        prompt
+      );
+      console.log(response);
+      return response.text;
+    }
   }
   customElements.define("query-chatgpt-widget", MainWebComponent);
 })();
