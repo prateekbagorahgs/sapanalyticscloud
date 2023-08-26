92f8fa0edb5a1561da9dd6dedc7c88ae95769c07
prateekbagorahgs <143021348+prateekbagorahgs@users.noreply.github.com>
2023-08-26 02:00:59 -0400

    Update QueryChatGPTWidget.js

--- QueryChatGPTWidget.js
+++ QueryChatGPTWidget.js
@@ -20,8 +20,8 @@ var ajaxCall = (prompt) => {
       const { response } = await ajaxCall(
         prompt
       );
-	console.log(response);
-	return response;
+	console.log(response.choices[0].text);
+	return response.choices[0].text;
     }
   }
   customElements.define("query-chatgpt-widget", MainWebComponent);
