3457870069fab27ed48fc7d2a1a2ad5fffa97f81
prateekbagorahgs <143021348+prateekbagorahgs@users.noreply.github.com>
2023-08-25 22:38:16 -0400

    Update QueryChatGPTWidget.js

--- QueryChatGPTWidget.js
+++ QueryChatGPTWidget.js
@@ -2,16 +2,16 @@
 	const template = document.createElement("template");
 
 	template.innerHTML = `
-      <div id="root" style="width: 100%; height: 100%;">
-      </div>
-    `;
+ 		<div id="root" style="width: 100%; height: 100%;">
+      		</div>
+    	`;
 	
 	class MainWebComponent extends HTMLElement {
 		constructor() {
-      super();
-      console.log("Hello!")
+      			super();
+      			console.log("Hello!")
 			console.log('dataBinding:', dataBinding);
 			}
   }
-  customElements.define("query_chatgpt-widget", MainWebComponent);
+  customElements.define("query-chatgpt-widget", MainWebComponent);
 })();
