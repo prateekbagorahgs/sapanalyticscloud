3649cbfbb111121de2032e83fea490cb10c0a7ee
prateekbagorahgs <143021348+prateekbagorahgs@users.noreply.github.com>
2023-08-25 22:36:25 -0400

    Update QueryChatGPTWidget.js

--- QueryChatGPTWidget.js
+++ QueryChatGPTWidget.js
@@ -1,2 +1,17 @@
-console.log("Hello!")
-console.log(JSON.stringify(this.dataBinding))
+(function() {
+	const template = document.createElement("template");
+
+	template.innerHTML = `
+      <div id="root" style="width: 100%; height: 100%;">
+      </div>
+    `;
+	
+	class MainWebComponent extends HTMLElement {
+		constructor() {
+      super();
+      console.log("Hello!")
+			console.log('dataBinding:', dataBinding);
+			}
+  }
+  customElements.define("query_chatgpt-widget", MainWebComponent);
+})();
