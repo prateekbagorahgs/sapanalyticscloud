469835a544f0c00c0c960c6a27ff197ff8554eda
prateekbagorahgs <143021348+prateekbagorahgs@users.noreply.github.com>
2023-08-26 15:10:34 -0400

    Update main.js

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ const url = "https://api.openai.com/v1";
     `;
   class MainWebComponent extends HTMLElement {
     async post(apiKey, endpoint, prompt) {
-      const prompt = JSON.parse(jsonString);
+      const prompt = JSON.parse(prompt);
       const { response } = await ajaxCall(
         apiKey,
         `${url}/${endpoint}`,
