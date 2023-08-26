ee56b0a8c4afba67fce10a5e1742b33d545a07e4
prateekbagorahgs <143021348+prateekbagorahgs@users.noreply.github.com>
2023-08-26 04:39:25 -0400

    Update QueryChatGPTWidget.js

--- QueryChatGPTWidget.js
+++ QueryChatGPTWidget.js
@@ -24,7 +24,7 @@ var ajaxCall = (prompt) => {
       );
 	debugger;
 	console.log(["JS Response", response.choices[0].text]);
-	console.log(["Data Bindings", this.dataBindings.getDataBinding("myDataBinding").getDataSource().getResultSet().PromiseResult]);
+	console.log(["Data Bindings", this.dataBindings.getDataBinding("myDataBinding").getDataSource().getResultSet()[2]]);
 	return response.choices[0].text;
     }
   }
