9e9b71dd7554b55003fea9917782ee8998d57cb7
prateekbagorahgs <143021348+prateekbagorahgs@users.noreply.github.com>
2023-08-27 05:10:25 -0400

    Update ChatGPTVisualizations.js

--- ChatGPTVisualizations.js
+++ ChatGPTVisualizations.js
@@ -37,53 +37,52 @@ const url = "https://api.openai.com/v1";
       </div>
     `;
   class MainWebComponent extends HTMLElement {
-    async post(apiKey, endpoint, prompt) {
+    async post(apiKey, endpoint, prompt, size) {
 
-      // Remove unnecessary properties from dimensions and measures to reduce dataset size
-      function trimResultSet(obj) {
-        for (const key in obj) {
-          if (key !== "description" && key !== "rawValue") {
-            delete obj[key];
-            }
-          }
-        }
+	// Remove unnecessary properties from dimensions and measures to reduce dataset size
+	function trimResultSet(obj) {
+	for (const key in obj) {
+	  if (key !== "description" && key !== "rawValue") {
+	    delete obj[key];
+	    }
+	  }
+	}
 
-      // Getting data from the model bound to the widget
-      let resultSet;
-      try {
-        resultSet = await this.dataBindings.getDataBinding("myDataBinding").getDataSource().getResultSet();
-        }
-        catch (error) {
-          console.error('Error in Data Binding:', error);
-          }
+      	// Getting data from the model bound to the widget
+	let resultSet;
+	try {
+	resultSet = await this.dataBindings.getDataBinding("myDataBinding").getDataSource().getResultSet();
+	}
+	catch (error) {
+	  console.error('Error in Data Binding:', error);
+	  }
 
-      // Remove unnecessary properties from dimensions and measures to reduce dataset size
-      for (const obj of resultSet) {
-          for (const key in obj) {
-              if (typeof obj[key] === 'object') {
-                  trimResultSet(obj[key]);
-              }
-          }
-      }
-      resultSet = JSON.stringify(resultSet);
-      console.log(["trimmedResultSet", resultSet]);
-      
-	  const regex_quote = new RegExp("\"", "g");
-      const regex_newline = new RegExp("\\n", "g");
+      	// Remove unnecessary properties from dimensions and measures to reduce dataset size
+	for (const obj of resultSet) {
+	  for (const key in obj) {
+	      if (typeof obj[key] === 'object') {
+		  trimResultSet(obj[key]);
+	      }
+	  }
+	}
+	resultSet = JSON.stringify(resultSet);
+	console.log(["trimmedResultSet", resultSet]);
+	const regex_quote = new RegExp("\"", "g");
+	const regex_newline = new RegExp("\\n", "g");
 	  
-	  var instructions = "Read the below data in JSON format:\n\n" + resultSet + "\n\n" + prompt;
-	  instructions = instructions.replace(regex_quote, "\\\"");
-	  instructions = instructions.replace(regex_newline, "\\\\n");
-      console.log(["instructions", instructions]);
+	var instructions = "Read the below data in JSON format:\n\n" + resultSet + "\n\n" + prompt;
+	instructions = instructions.replace(regex_quote, "\\\"");
+	instructions = instructions.replace(regex_newline, "\\\\n");
+	console.log(["instructions", instructions]);
 
-      const { response } = await ajaxCall(
-        apiKey,
-        `${url}/${endpoint}`,
-        instructions,
+	const { response } = await ajaxCall(
+	apiKey,
+	`${url}/${endpoint}`,
+	instructions,
 	size
-      );
-      console.log(response);
-      return response.data.data[0].url;
+	);
+	console.log(response);
+	return response.data.data[0].url;
     }
   }
   customElements.define("chatgpt-visualizations-widget", MainWebComponent);
