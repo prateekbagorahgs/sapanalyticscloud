5435aab0efc429c879d965721e1888c862960c2d
prateekbagorahgs <143021348+prateekbagorahgs@users.noreply.github.com>
2023-08-27 05:40:28 -0400

    Update ChatGPTVisualizations.js

--- ChatGPTVisualizations.js
+++ ChatGPTVisualizations.js
@@ -2,15 +2,15 @@ var ajaxCall = (key, url, prompt, size) => {
   return new Promise((resolve, reject) => {
     $.ajax({
       url: url,
-      type: "POST",
-      dataType: "json",
+      type: 'POST',
+      dataType: 'json',
       data: JSON.stringify({
         prompt: prompt,
-		size: size,
+        size: size,
         n: 1,
       }),
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
         Authorization: `Bearer ${key}`,
       },
       crossDomain: true,
@@ -26,10 +26,10 @@ var ajaxCall = (key, url, prompt, size) => {
   });
 };
 
-const url = "https://api.openai.com/v1";
+const url = 'https://api.openai.com/v1';
 
 (function () {
-  const template = document.createElement("template");
+  const template = document.createElement('template');
   template.innerHTML = `
       <style>
       </style>
@@ -38,53 +38,55 @@ const url = "https://api.openai.com/v1";
     `;
   class MainWebComponent extends HTMLElement {
     async post(apiKey, endpoint, prompt, size) {
-
       // Remove unnecessary properties from dimensions and measures to reduce dataset size
       function trimResultSet(obj) {
         for (const key in obj) {
-          if (key !== "description" && key !== "rawValue") {
+          if (key !== 'description' && key !== 'rawValue') {
             delete obj[key];
-            }
           }
         }
+      }
 
       // Getting data from the model bound to the widget
       let resultSet;
       try {
-        resultSet = await this.dataBindings.getDataBinding("myDataBinding").getDataSource().getResultSet();
-        }
-        catch (error) {
-          console.error('Error in Data Binding:', error);
-          }
+        resultSet = await this.dataBindings
+          .getDataBinding('myDataBinding')
+          .getDataSource()
+          .getResultSet();
+      } catch (error) {
+        console.error('Error in Data Binding:', error);
+      }
 
       // Remove unnecessary properties from dimensions and measures to reduce dataset size
       for (const obj of resultSet) {
-          for (const key in obj) {
-              if (typeof obj[key] === 'object') {
-                  trimResultSet(obj[key]);
-              }
+        for (const key in obj) {
+          if (typeof obj[key] === 'object') {
+            trimResultSet(obj[key]);
           }
+        }
       }
       resultSet = JSON.stringify(resultSet);
-      console.log(["trimmedResultSet", resultSet]);
-      
-	  const regex_quote = new RegExp("\"", "g");
-      const regex_newline = new RegExp("\\n", "g");
-	  
-	  var instructions = "Read the below data in JSON format:\n\n" + resultSet + "\n\n" + prompt;
-	  instructions = instructions.replace(regex_quote, "\\\"");
-	  instructions = instructions.replace(regex_newline, "\\\\n");
-      console.log(["instructions", instructions]);
+      console.log(['trimmedResultSet', resultSet]);
+
+      const regex_quote = new RegExp('"', 'g');
+      const regex_newline = new RegExp('\\n', 'g');
+
+      var instructions =
+        'Read the below data in JSON format:\n\n' + resultSet + '\n\n' + prompt;
+      instructions = instructions.replace(regex_quote, '\\"');
+      instructions = instructions.replace(regex_newline, '\\\\n');
+      console.log(['instructions', instructions]);
 
       const { response } = await ajaxCall(
         apiKey,
         `${url}/${endpoint}`,
         instructions,
-		size
+        size
       );
       console.log(response);
       return response.data.data[0].url;
     }
   }
-  customElements.define("chatgpt-visualizations-widget", MainWebComponent);
+  customElements.define('chatgpt-visualizations-widget', MainWebComponent);
 })();
