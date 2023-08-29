dfd12436f8a492aa6ad7f6a86b37f3da969dce72
prateekbagorahgs <143021348+prateekbagorahgs@users.noreply.github.com>
2023-08-29 16:50:25 -0400

    Update ChatGPTPyodide.js

--- ChatGPTPyodide.js
+++ ChatGPTPyodide.js
@@ -1,35 +1,5 @@
-var ajaxCall = (key, url, prompt, size) => {
-  return new Promise((resolve, reject) => {
-    $.ajax({
-      url: url,
-      type: 'POST',
-      dataType: 'json',
-      data: JSON.stringify({
-        prompt: prompt,
-        size: size,
-        n: 1,
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${key}`,
-      },
-      crossDomain: true,
-      success: function (response, status, xhr) {
-        resolve({ response, status, xhr });
-      },
-      error: function (xhr, status, error) {
-        const err = new Error('xhr error');
-        err.status = xhr.status;
-        reject(err);
-      },
-    });
-  });
-};
-
-const url = 'https://api.openai.com/v1';
-
 (function () {
-  const template = document.createElement('template');
+  const template = document.createElement("template");
   template.innerHTML = `
       <style>
       </style>
@@ -37,56 +7,102 @@ const url = 'https://api.openai.com/v1';
       </div>
     `;
   class MainWebComponent extends HTMLElement {
-    async post(apiKey, endpoint, prompt, size) {
-      // Remove unnecessary properties from dimensions and measures to reduce dataset size
-      function trimResultSet(obj) {
-        for (const key in obj) {
-          if (key !== 'description' && key !== 'rawValue') {
-            delete obj[key];
+
+	constructor () {
+      super();
+      this.pyodide = null;
+      this.messageArray = null;
+      this.fetchPyodide();
+      this.fetchResultSet();
+    }
+
+    // Funtion to load pyodide
+    async fetchPyodide () {
+      await getScriptPromisify('https://cdn.jsdelivr.net/pyodide/v0.23.4/full/pyodide.js')
+      const pyodide = await loadPyodide()
+      this.pyodide = pyodide
+    }
+
+    // Function to run python code in pyodide
+    async runPythonCode(code){
+      const codeOutput = await this.pyodide.runPythonAsync(code);
+      return codeOutput;
+    }
+
+    // Function to remove unnecessary properties from dimensions and measures to reduce dataset size
+    function trimResultSet(obj) {
+      for (const key in obj) {
+        if (key !== "description" && key !== "rawValue") {
+          delete obj[key];
           }
         }
       }
+
+    // Function for getting data from the model bound to the widget
+    async function fetchResultSet() {
+      try {
+        resultSet = await this.dataBindings.getDataBinding("myDataBinding").getDataSource().getResultSet();
+        
+        // Remove unnecessary properties from dimensions and measures to reduce dataset size
+        for (const obj of resultSet) {
+            for (const key in obj) {
+                if (typeof obj[key] === 'object') {
+                    this.trimResultSet(obj[key]);
+                }
+            }
+        }
+        this.resultSet = JSON.stringify(resultSet);
+      }
+        catch (error) {
+          console.error('Error in Fetching Dataset:', error);
+          }
+      }
+    
+    // Function for getting data from the model bound to the widget
+    async function prepareMessages() {
+      try {
+        let messageArray = [];
+        
+        const regex_quote = new RegExp("\"", "g");
+        const regex_newline = new RegExp("\\n", "g");
+  
+        // Managing conversation history to maintain session
+        // The first message contains dataset in JSON format and instructions to ChatGPT
+        var instructions = "You are my laconic assistant. Read the below data in JSON format from SAP Analytics Cloud. Only answer compact python code <code> to determine the answer to further questions with no other text. I will myself pass it to exec(<code>, {'json_data', json.loads(json_data)}), where json_data = " + this.resultSet + "\n\nStore the final result in variable 'output'.";
+        instructions = instructions.replace(regex_quote, "\\\"");
+        var firstMessage = '{"role": "system", "content": "' + instructions + '"}';
+        const messageObject = JSON.parse(firstMessage.replace(regex_newline, "\\\\n"));        
+        messageArray.push(messageObject);
+
+        for (const promptString of prompt) {
+          try {
+            const messageObject = JSON.parse(promptString.replace(regex_newline, "\\\\n"));
+            messageArray.push(messageObject);
+            } catch (error) {
+            console.error('Error parsing Prompt JSON:', error);
+            }
+          }
+        console.log(["messageArray", messageArray]);
+        } catch (error) {
+        console.error('Error in preparing Messages:', error);
+        }
+      }
+    
+    async post(apiKey, endpoint, prompt) {
 
       // Getting data from the model bound to the widget
-      let resultSet;
-      try {
-        resultSet = await this.dataBindings
-          .getDataBinding('myDataBinding')
-          .getDataSource()
-          .getResultSet();
-      } catch (error) {
-        console.error('Error in Data Binding:', error);
-      }
+      const messageArray = await this.prepareMessages();
+      console.log("messageArray", messageArray);
 
-      // Remove unnecessary properties from dimensions and measures to reduce dataset size
-      for (const obj of resultSet) {
-        for (const key in obj) {
-          if (typeof obj[key] === 'object') {
-            trimResultSet(obj[key]);
-          }
-        }
-      }
-      resultSet = JSON.stringify(resultSet);
-      console.log(['trimmedResultSet', resultSet]);
-
-      const regex_quote = new RegExp('"', 'g');
-      const regex_newline = new RegExp('\\n', 'g');
-
-      var instructions =
-        'Read the below data in JSON format:\n\n' + resultSet + '\n\n' + prompt;
-      instructions = instructions.replace(regex_quote, '\\"');
-      instructions = instructions.replace(regex_newline, '\\\\n');
-      console.log(['instructions', instructions]);
-
+      // API call to ChatGPT
       const { response } = await ajaxCall(
         apiKey,
         `${url}/${endpoint}`,
-        instructions,
-        size
+        messageArray
       );
       console.log(response);
-      return response.data.data[0].url;
+      const output = response.choices[0].message.content.replace(/["']/g, '');
+      console.log(output);
+      return output;
     }
   }
-  customElements.define('chatgpt-pyodide-widget', MainWebComponent);
-})();
