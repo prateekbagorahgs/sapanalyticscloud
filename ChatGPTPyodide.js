4291a3912bbf425e390e1b694983a3ec6dffcef9
prateekbagorahgs <143021348+prateekbagorahgs@users.noreply.github.com>
2023-08-29 20:03:56 -0400

    Update ChatGPTPyodide.js

--- ChatGPTPyodide.js
+++ ChatGPTPyodide.js
@@ -1,154 +1,159 @@
 var getScriptPromisify = (src) => {
-  return new Promise(resolve => {
-    $.getScript(src, resolve);
-  })
+    return new Promise(resolve => {
+        $.getScript(src, resolve);
+    })
 }
 
 var ajaxCall = (key, url, messages) => {
-  return new Promise((resolve, reject) => {
-    $.ajax({
-      url: url,
-      type: "POST",
-      dataType: "json",
-      data: JSON.stringify({
-        // model: "gpt-3.5-turbo-0613",
-        // max_tokens: 1024,
-        model: "gpt-4",
-        max_tokens: 4096,
-        messages: messages,
-        n: 1,
-        temperature: 0.3,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${key}`,
-      },
-      crossDomain: true,
-      success: function (response, status, xhr) {
-        resolve({ response, status, xhr });
-      },
-      error: function (xhr, status, error) {
-        const err = new Error('xhr error');
-        err.status = xhr.status;
-        reject(err);
-      },
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            url: url,
+            type: "POST",
+            dataType: "json",
+            data: JSON.stringify({
+                // model: "gpt-3.5-turbo-0613",
+                // max_tokens: 1024,
+                model: "gpt-4",
+                max_tokens: 4096,
+                messages: messages,
+                n: 1,
+                temperature: 0.3,
+            }),
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${key}`,
+            },
+            crossDomain: true,
+            success: function(response, status, xhr) {
+                resolve({
+                    response,
+                    status,
+                    xhr
+                });
+            },
+            error: function(xhr, status, error) {
+                const err = new Error('xhr error');
+                err.status = xhr.status;
+                reject(err);
+            },
+        });
     });
-  });
 };
 
 const url = "https://api.openai.com/v1";
 
-(function () {
-  const template = document.createElement("template");
-  template.innerHTML = `
+(function() {
+    const template = document.createElement("template");
+    template.innerHTML = `
       <style>
       </style>
       <div id="root" style="width: 100%; height: 100%;">
       </div>
     `;
-  class MainWebComponent extends HTMLElement {
-    constructor () {
-      super();
-      this.pyodide = null;
-      this.fetchPyodide();
-    }
-
-    // Funtion to load pyodide
-    async fetchPyodide () {
-      await getScriptPromisify('https://cdn.jsdelivr.net/pyodide/v0.23.4/full/pyodide.js');
-      const pyodide = await loadPyodide();
-      this.pyodide = pyodide;
-      console.log(["pyodide", this.pyodide]);
-    }
-
-    // Function to run python code in pyodide
-    async runPythonCode(code){
-      const codeOutput = await this.pyodide.runPythonAsync(code);
-      console.log(["codeOutput", codeOutput]);
-      return codeOutput;
-    }
-
-    // Function to remove unnecessary properties from dimensions and measures to reduce dataset size
-    trimResultSet(obj) {
-      for (const key in obj) {
-        if (key !== "description" && key !== "rawValue") {
-          delete obj[key];
-          }
+    class MainWebComponent extends HTMLElement {
+        constructor() {
+            super();
+            this.pyodide = null;
+            this.fetchPyodide();
         }
-      }
 
-    // Function for getting data from the model bound to the widget
-    async fetchResultSet() {
-      try {
-        var resultSet = await this.dataBindings.getDataBinding("myDataBinding").getDataSource().getResultSet();
-        
-        // Remove unnecessary properties from dimensions and measures to reduce dataset size
-        for (const obj of resultSet) {
+        // Funtion to load pyodide
+        async fetchPyodide() {
+            await getScriptPromisify('https://cdn.jsdelivr.net/pyodide/v0.23.4/full/pyodide.js');
+            const pyodide = await loadPyodide();
+            this.pyodide = pyodide;
+            console.log(["pyodide", this.pyodide]);
+        }
+
+        // Function to run python code in pyodide
+        async runPythonCode(code) {
+            const codeOutput = await this.pyodide.runPythonAsync(code);
+            console.log(["codeOutput", codeOutput]);
+            return codeOutput;
+        }
+
+        // Function to remove unnecessary properties from dimensions and measures to reduce dataset size
+        trimResultSet(obj) {
             for (const key in obj) {
-                if (typeof obj[key] === 'object') {
-                    this.trimResultSet(obj[key]);
+                if (key !== "description" && key !== "rawValue") {
+                    delete obj[key];
                 }
             }
         }
-        resultSet = JSON.stringify(resultSet);
-		return resultSet
-        console.log(["resultSet", resultSet]);
-      }
-        catch (error) {
-          console.error('Error in Fetching Dataset:', error);
-          }
-      }
-    
-    // Function for getting data from the model bound to the widget
-    async prepareMessages(resultSet, prompt) {
-      try {
-        let messageArray = [];
 
-        const regex_quote = new RegExp("\"", "g");
-        const regex_newline = new RegExp("\\n", "g");
-  
-        // Managing conversation history to maintain session
-        // The first message contains dataset in JSON format and instructions to ChatGPT
-        var instructions = "You are my laconic assistant. Read the below data in JSON format from SAP Analytics Cloud. Only answer compact python code <code> to determine the answer to further questions with no other text. I will myself pass it to exec(<code>, {'json_data', json.loads(json_data)}), where json_data = " + resultSet + "\n\nStore the final result in variable 'output'.";
-        instructions = instructions.replace(regex_quote, "\\\"");
-        var firstMessage = '{"role": "system", "content": "' + instructions + '"}';
-        const messageObject = JSON.parse(firstMessage.replace(regex_newline, "\\\\n"));        
-        messageArray.push(messageObject);
+        // Function for getting data from the model bound to the widget
+        async fetchResultSet() {
+            try {
+                var resultSet = await this.dataBindings.getDataBinding("myDataBinding").getDataSource().getResultSet();
 
-        for (const promptString of prompt) {
-          try {
-            const messageObject = JSON.parse(promptString.replace(regex_newline, "\\\\n"));
-            messageArray.push(messageObject);
+                // Remove unnecessary properties from dimensions and measures to reduce dataset size
+                for (const obj of resultSet) {
+                    for (const key in obj) {
+                        if (typeof obj[key] === 'object') {
+                            this.trimResultSet(obj[key]);
+                        }
+                    }
+                }
+                resultSet = JSON.stringify(resultSet);
+                return resultSet
+                console.log(["resultSet", resultSet]);
             } catch (error) {
-            console.error('Error parsing Prompt JSON:', error);
+                console.error('Error in Fetching Dataset:', error);
             }
-          }
-        return messageArray;
-        console.log(["messageArray", messageArray]);
-        } catch (error) {
-        console.error('Error in preparing Messages:', error);
         }
-      }
-    
-    async post(apiKey, endpoint, prompt) {
 
-      // Getting data from the model bound to the widget
-	  const resultSet = await this.fetchResultSet();
-      console.log(["resultSet", resultSet]);
-      const messageArray = await this.prepareMessages(resultSet, prompt);
-      console.log("messageArray post", messageArray);
+        // Function for getting data from the model bound to the widget
+        async prepareMessages(resultSet, prompt) {
+            try {
+                let messageArray = [];
 
-      // API call to ChatGPT
-      const { response } = await ajaxCall(
-        apiKey,
-        `${url}/${endpoint}`,
-        messageArray
-      );
-      console.log(response);
-      const output = response.choices[0].message.content.replace(/["']/g, '');
-      console.log(output);
-      return output;
+                const regex_quote = new RegExp("\"", "g");
+                const regex_newline = new RegExp("\\n", "g");
+
+                // Managing conversation history to maintain session
+                // The first message contains dataset in JSON format and instructions to ChatGPT
+                var instructions = "You are my laconic assistant. Read the below data in JSON format from SAP Analytics Cloud. Only answer compact python code <code> to determine the answer to further questions with no other text. I will myself pass it to exec(<code>, {'json_data', json.loads(json_data)}), where json_data = " + resultSet + "\n\nStore the final result in variable 'output'.";
+                instructions = instructions.replace(regex_quote, "\\\"");
+                var firstMessage = '{"role": "system", "content": "' + instructions + '"}';
+                const messageObject = JSON.parse(firstMessage.replace(regex_newline, "\\\\n"));
+                messageArray.push(messageObject);
+
+                for (const promptString of prompt) {
+                    try {
+                        const messageObject = JSON.parse(promptString.replace(regex_newline, "\\\\n"));
+                        messageArray.push(messageObject);
+                    } catch (error) {
+                        console.error('Error parsing Prompt JSON:', error);
+                    }
+                }
+                return messageArray;
+                console.log(["messageArray", messageArray]);
+            } catch (error) {
+                console.error('Error in preparing Messages:', error);
+            }
+        }
+
+        async post(apiKey, endpoint, prompt) {
+
+            // Getting data from the model bound to the widget
+            const resultSet = await this.fetchResultSet();
+            console.log(["resultSet", resultSet]);
+            const messageArray = await this.prepareMessages(resultSet, prompt);
+            console.log("messageArray post", messageArray);
+
+            // API call to ChatGPT
+            const {
+                response
+            } = await ajaxCall(
+                apiKey,
+                `${url}/${endpoint}`,
+                messageArray
+            );
+            console.log(response);
+            const output = response.choices[0].message.content.replace(/["']/g, '');
+            console.log(output);
+            return output;
+        }
     }
-  }
-  customElements.define("chatgpt-databindings-widget", MainWebComponent);
+    customElements.define("chatgpt-databindings-widget", MainWebComponent);
 })();
