36aaf4db06509b3d39b0a557d4f0cc4763617ddd
prateekbagorahgs <143021348+prateekbagorahgs@users.noreply.github.com>
2023-08-31 00:55:00 -0400

    Update ChatGPTPyodide.js

--- ChatGPTPyodide.js
+++ ChatGPTPyodide.js
@@ -53,6 +53,9 @@ const url = "https://api.openai.com/v1";
     class MainWebComponent extends HTMLElement {
         constructor() {
             super();
+            this.resultSet = null;
+            this.sampleSet = null;
+            this.codePython = this.fetchPythonCode();
             this.pyodide = null;
             this.fetchPyodide();
         }
@@ -68,6 +71,14 @@ const url = "https://api.openai.com/v1";
             } catch (error) {
                 console.error("Unable to load pyodide: ", error);
             }
+        }
+
+        // Function to create executable pyhton code
+        fetchPythonCode() {
+            var codePython = "import json;\n";
+            codePython = codePython + "exec(codeChatGPT, globals())\n";
+            codePython = codePython + "print(output)";
+            return codePython;
         }
 
         // Function to remove unnecessary properties from dimensions and measures to reduce dataset size
@@ -92,15 +103,50 @@ const url = "https://api.openai.com/v1";
                         }
                     }
                 }
-                resultSet = JSON.stringify(resultSet);
                 return resultSet;
             } catch (error) {
                 console.error("Unable to fetch dataset: ", error);
             }
         }
 
+        generateDummyValue(value) {
+            if (typeof value === "string") {
+                return "abcd";
+                } else if (typeof value === "number") {
+                return 9999;
+                } else if (typeof value === "boolean") {
+                return true;
+                }
+            return null;
+            }
+
+        replaceWithDummyValue(element) {
+            if (typeof element === "object" && !Array.isArray(element)) {
+                for (const key in element) {
+                    if (typeof element[key] === "object") {
+                        this.replaceWithDummy(element[key]);
+                        } else {
+                        element[key] = this.generateDummyValue(element[key]);
+                        }
+                    }
+                }
+            }
+
+        // Derive a sample structure of data
+        fetchSampleSet() {
+            try {
+                const firstElement = this.resultSet[0];
+                for (const element of jsonArray) {
+                    this.replaceWithDummy(element);
+                    }
+                return sampleSet;
+                } catch (error) {
+                console.error("Dataset is probably empty: ", error);
+                }
+            }
+
         // Function for getting data from the model bound to the widget
-        async prepareMessages(resultSet, prompt) {
+        prepareMessages(prompt) {
             try {
                 let messageArray = [];
 
@@ -109,7 +155,7 @@ const url = "https://api.openai.com/v1";
 
                 // Managing conversation history to maintain session
                 // The first message contains dataset in JSON format and instructions to ChatGPT
-                var instructions = "You are my laconic python developer. Read the below data in JSON format from SAP Analytics Cloud. Only answer compact pyodide python code <code> to determine the answer to further questions. I will myself pass it to exec(<code>, {'json_data', json.loads(json_data)}), where json_data = " + resultSet + "The <code> should store the final result in variable 'output' as a descriptive string understandable to business users.";
+                var instructions = "You are my laconic python developer. Refer to below JSON format of json_data from SAP Analytics Cloud. Only answer compact pyodide python code <code> to determine the answer to further questions. I will myself pass it to exec(<code>, {'json_data', json.loads(json_data)}).\n\nHere, json_data looks like " + this.sampleSet + "The <code> should store the final result in variable 'output' as a descriptive string understandable to business users.";
                 instructions = instructions.replace(regex_quote, "\\\"");
                 var firstMessage = '{"role": "system", "content": "' + instructions + '"}';
                 var messageObject = JSON.parse(firstMessage.replace(regex_newline, "\\\\n"));
@@ -129,22 +175,13 @@ const url = "https://api.openai.com/v1";
             }
         }
 
-        // Function to create executable pyhton code
-        async fetchExecutableCode() {
-            var codePython = "import json;\n";
-            codePython = codePython + "json_data = json.loads(resultSet)\n";
-            codePython = codePython + "exec(codeChatGPT, globals())\n";
-            codePython = codePython + "print(output)";
-            return codePython;
-        }
-
         // Function to run python code in pyodide
-        async runPythonCode(resultSet, codeChatGPT, codePython) {
-            this.pyodide.globals.set("resultSet", resultSet);
+        async runPythonCode(codeChatGPT) {
+            this.pyodide.globals.set("resultSet", this.resultSet);
             this.pyodide.globals.set("codeChatGPT", codeChatGPT);
             this.pyodide.globals.set("output", "");
             try {
-                await this.pyodide.runPythonAsync(codePython);
+                await this.pyodide.runPythonAsync(this.codePython);
             } catch (error) {
                 console.error("Could not execute pyodide code: ", error);
             }
@@ -152,11 +189,19 @@ const url = "https://api.openai.com/v1";
 
         // Main function
         async post(apiKey, endpoint, prompt) {
-
             try {
+
                 // Getting data from the model bound to the widget
-                const resultSet = await this.fetchResultSet();
-                const messageArray = await this.prepareMessages(resultSet, prompt);
+                if (this.resultSet === null) {
+                    this.resultSet = await this.fetchResultSet();
+                    this.sampleSet = this.fetchSampleSet();
+                    console.log(["resultSet", this.resultSet]);
+                    console.log(["sampleSet", this.sampleSet]);   
+                }
+
+                // Prepare messages for ChatGPT
+                const messageArray = await this.prepareMessages(prompt);
+                console.log(["messageArray", messageArray]);
 
                 // API call to ChatGPT
                 const {
@@ -169,16 +214,17 @@ const url = "https://api.openai.com/v1";
                 const codeChatGPT = response.choices[0].message.content;
                 console.log(["codeChatGPT", codeChatGPT]);
 
-                // Fetch executable python code
-                const codePython = await this.fetchExecutableCode();
+                // const codeChatGPT = `output = {item['Vendor']['description']: 0 for item in json_data if item['@MeasureDimension']['description'] == 'Order Qty'}\nfor item in json_data:\n    if item['@MeasureDimension']['description'] == 'Order Qty':\n        output[item['Vendor']['description']] += int(item['@MeasureDimension']['rawValue'])\noutput = ', '.join([f'{k}: {v}' for k, v in output.items()])`;
 
                 // Execte python code in pyodide
-                await this.runPythonCode(resultSet, codeChatGPT, codePython);
+                await this.runPythonCode(codeChatGPT);
 
                 // Get output from pyodide environment
                 const codeOutput = this.pyodide.globals.get("output");
                 console.log(["codeOutput", codeOutput]);
+
                 return codeOutput;
+
             } catch (error) {
                 console.error("Could not execute the post request: ", error);
             }
